63652e24b0023f68001c98e95e7b6401adbce9a0
llll064299050ll33322 <muha1gryaznuha@gmail.com>
2025-10-25 13:12:26 +0300

    Add files via upload

---
+++ MainDZ6.java
@@ -0,0 +1,17 @@
+public class MainDZ6 {
+    public static void main(String[] args) {
+
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int i = 0; i < numbers.length; i++) {
+        numbers[i] = numbers[i] * 2;
+    }
+
+    System.out.println("Измененный массив, у которого элементы увеличены в два раза: ");
+
+    for (int number : numbers) {
+        System.out.print(number + " ");
+    }
+
+}
+}
