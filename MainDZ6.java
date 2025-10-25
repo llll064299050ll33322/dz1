4f71a24c88785d183754b7b4a21b03693eb0c6c5
llll064299050ll33322 <muha1gryaznuha@gmail.com>
2025-10-25 13:21:48 +0300

    Update MainDZ6.java

--- MainDZ6.java
+++ MainDZ6.java
@@ -3,11 +3,16 @@ public class MainDZ6 {
 
     int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+System.out.println("Исходный массив:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
     for (int i = 0; i < numbers.length; i++) {
         numbers[i] = numbers[i] * 2;
     }
 
-    System.out.println("Измененный массив, у которого элементы увеличены в два раза: ");
+    System.out.println("\nИзмененный массив, у которого элементы увеличены в два раза: ");
 
     for (int number : numbers) {
         System.out.print(number + " ");
